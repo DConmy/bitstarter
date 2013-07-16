28f650fece0f523af1fc318864f1fe78e8ff0231
Doug Conmy <dconmy@gmail.com>
2013-07-16 01:37:18 +0000

    Updated web.js with index reading[B

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  buffer = fs.readFileSync("index.html")
+  theString = buffer.toString();  
+  response.send(theStrings);
 });
 
 var port = process.env.PORT || 5000;
